24df666e9491a851ae0252a6dcc44e93f46a2c45
Git-Romer <Git-Romer@users.noreply.github.com>
2026-05-23 17:29:44 +0200

    Add more horizontal page padding

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
   return (
     <div className="min-h-dvh flex flex-col bg-bg overflow-x-hidden">
       {!isHome && <AppNav />}
-      <main className={`flex-1 ${!isHome ? 'w-full px-4 pb-8' : ''}`}>
+      <main className={`flex-1 ${!isHome ? 'w-full px-5 pb-8 sm:px-6 lg:px-8' : ''}`}>
         <Outlet />
       </main>
     </div>
